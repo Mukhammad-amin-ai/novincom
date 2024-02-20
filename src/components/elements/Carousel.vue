c8635aad5c1374835aa4ab26488cab1c4915759f
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2024-02-20 22:24:17 +0500

    update prev next buttons

--- src/components/elements/Carousel.vue
+++ src/components/elements/Carousel.vue
@@ -9,7 +9,6 @@
         :space-between="150"
         @slideChange="onSlideChange"
         @swiper="onSwiper"
-        :pagination="true"
         :autoplay="{
           delay: 2500,
           disableOnInteraction: false,
@@ -40,8 +39,22 @@
               </div>
 
               <div class="w-100 d-flex justify-content-start">
-                <router-link to="'auth/login'" class="buy-btn text-white mb-2"
+                <router-link to="auth/login" class="auth-btn text-white mb-2"
                   >Вход / Регистрация
+                  <svg
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M1 7.99999C1 7.86738 1.05268 7.74021 1.14645 7.64644C1.24021 7.55267 1.36739 7.49999 1.5 7.49999H13.293L10.146 4.35399C10.0521 4.2601 9.99937 4.13277 9.99937 3.99999C9.99937 3.86721 10.0521 3.73988 10.146 3.64599C10.2399 3.5521 10.3672 3.49936 10.5 3.49936C10.6328 3.49936 10.7601 3.5521 10.854 3.64599L14.854 7.64599C14.9006 7.69244 14.9375 7.74761 14.9627 7.80836C14.9879 7.8691 15.0009 7.93422 15.0009 7.99999C15.0009 8.06576 14.9879 8.13088 14.9627 8.19162C14.9375 8.25237 14.9006 8.30754 14.854 8.35399L10.854 12.354C10.7601 12.4479 10.6328 12.5006 10.5 12.5006C10.3672 12.5006 10.2399 12.4479 10.146 12.354C10.0521 12.2601 9.99937 12.1328 9.99937 12C9.99937 11.8672 10.0521 11.7399 10.146 11.646L13.293 8.49999H1.5C1.36739 8.49999 1.24021 8.44731 1.14645 8.35354C1.05268 8.25978 1 8.1326 1 7.99999Z"
+                      fill="white"
+                    />
+                  </svg>
                 </router-link>
               </div>
             </div>
@@ -108,20 +121,20 @@ export default {
 <style>
 .swiper-button-next,
 .swiper-button-prev {
-  background: #fff;
+  background: var(--blue);
   border-radius: 50px;
   height: 40px;
   width: 40px;
   display: flex;
   justify-content: center !important;
   align-items: center !important;
-  border: 1px solid #9dadcb;
+  border: 1px solid transparent;
 }
 .swiper-button-next::after {
   position: absolute;
   top: 5%;
   left: 20%;
-  color: #5c6ac4 !important;
+  color: white !important;
   width: 10px;
   height: 10px;
   padding: 10px;
@@ -132,7 +145,7 @@ export default {
   position: absolute;
   top: 5%;
   left: 10%;
-  color: #5c6ac4 !important;
+  color: white !important;
   width: 10px;
   height: 10px;
   padding: 10px;
@@ -160,7 +173,7 @@ export default {
     position: absolute;
     top: 25%;
     left: 13px !important;
-    color: #5c6ac4 !important;
+    color: white !important;
     width: 8px;
     height: 8px;
     padding: 0 !important;
@@ -170,7 +183,7 @@ export default {
     top: 25%;
     right: 13px !important;
     left: auto !important;
-    color: #5c6ac4 !important;
+    color: #fff !important;
     width: 8px !important;
     padding: 0px !important;
   }
