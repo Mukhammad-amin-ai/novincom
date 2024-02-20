7760463bbeab6e7cb9475d2b9ca1a82adea5e91c
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2024-02-20 20:34:01 +0500

    footer

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
   <div>
     <!-- <HeaderDefault/> -->
       <RouterView/>
+    <FooterDefault/>
   </div>
 </template>
 <script setup>
 import HeaderDefault from './components/partial/headers/HeaderDefault.vue';
-
+import FooterDefault from './components/partial/footers/FooterDefault.vue';
 
 </script>
 
