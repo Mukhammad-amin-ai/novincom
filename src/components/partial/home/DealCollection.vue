c60ccc247623d6acdbfb8cce8fc5ecadcf74d29f
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2024-02-21 16:33:00 +0500

    chnage

--- src/components/partial/home/DealCollection.vue
+++ src/components/partial/home/DealCollection.vue
@@ -1,100 +1,74 @@
 <template>
   <div class="container">
     <div class="heading text-center mb-3">
-      <h2 class="title">Deals & Outlet</h2>
-
-      <p class="title-desc">Today’s deal and more</p>
+      <h2 class="title">Популярные направления из Москвы</h2>
+      <p class="title-desc">Предложения дня и распродажи</p>
     </div>
-
     <div class="row">
       <div class="col-lg-6 deal-col">
-        <div
-          class="deal"
-          v-lazy:background-image="'./images/home/deals/bg-1.jpg'"
-        >
+        <div class="deal deal1">
           <div class="deal-top">
-            <h2>Deal of the Day.</h2>
-            <h4>Limited quantities.</h4>
+            <h2 style="font-family: Gilroy-Medium !important;">Санкт-Петербург</h2>
+            <h4>Авиабилеты</h4>
           </div>
-
           <div class="deal-content">
             <h3 class="product-title">
-              <router-link
-                to="/product/default/home-smart-speaker-with-google-assistant"
-                >Home Smart Speaker with Google Assistant</router-link
-              >
+              <router-link to="/product/default/home-smart-speaker-with-google-assistant"
+                style="font-family: Gilroy-Medium !important;">Туда - обратно</router-link>
             </h3>
-
             <div class="product-price">
-              <span class="new-price">$129.99</span>
-              <span class="old-price">Was $159.99</span>
+              <span class="new-price" style="font-family: Gilroy-Medium !important;">12 159.99
+                бонусов</span>
+              <!-- <span class="old-price">Was $159.99</span> -->
             </div>
-
-            <router-link
-              to="/product/default/home-smart-speaker-with-google-assistant"
-              class="btn btn-link"
-            >
-              <span>Shop Now</span>
-              <i class="icon-long-arrow-right"></i>
+            <router-link to="/product/default/home-smart-speaker-with-google-assistant" class="btn btn-link"
+              style="gap: 5px; margin-top: 40px;">
+              <span style="font-family: Gilroy-Medium !important;">Бронировать</span>
+              <img src="../../../assets/newImg/icons/bi_arrow-right.svg" alt="">
+              <!-- <i class="icon-long-arrow-right"></i> -->
             </router-link>
           </div>
-
           <div class="deal-bottom">
-            <count-down
+            <!-- <count-down
               wrap="deal-countdown daily-deal-countdown"
               until="+10h"
               format="HMS"
-            ></count-down>
+            ></count-down> -->
           </div>
         </div>
       </div>
-
       <div class="col-lg-6 deal-col">
-        <div
-          class="deal"
-          v-lazy:background-image="'./images/home/deals/bg-2.jpg'"
-        >
+        <div class="deal deal2">
           <div class="deal-top">
-            <h2>Your Exclusive Offers.</h2>
-            <h4>Sign in to see amazing deals.</h4>
+            <h2 style="font-family: Gilroy-Medium !important;">Калининград</h2>
+            <h4>Авиабилеты</h4>
           </div>
-
           <div class="deal-content">
             <h3 class="product-title">
-              <router-link
-                to="/product/default/microsoft-refurbish-xbox-one-s-500gb"
-                >Microsoft – Refurbish Xbox One S 500GB</router-link
-              >
+              <router-link to="/product/default/microsoft-refurbish-xbox-one-s-500gb">В одну сторону</router-link>
             </h3>
-
             <div class="product-price">
-              <span class="new-price">$29.99</span>
+              <span class="new-price">7 569.99
+                бонусов</span>
             </div>
-
-            <router-link to="/pages/login" class="btn btn-link">
-              <span>Sign In and Save money</span>
-              <i class="icon-long-arrow-right"></i>
+            <router-link to="/pages/login" class="btn btn-link" style="gap: 5px; margin-top: 40px;">
+              <span style="font-family: Gilroy-Medium !important;">Бронировать</span>
+              <img src="../../../assets/newImg/icons/bi_arrow-right.svg" alt="">
+              <!-- <i class="icon-long-arrow-right"></i> -->
             </router-link>
           </div>
-
           <div class="deal-bottom">
-            <count-down
-              wrap="deal-countdown daily-deal-countdown"
-              until="+11h"
-              format="HMS"
-            ></count-down>
+            <!-- <count-down wrap="deal-countdown daily-deal-countdown" until="+11h" format="HMS"></count-down> -->
           </div>
         </div>
       </div>
     </div>
-
     <div class="more-container text-center mt-1 mb-5">
-      <router-link
-        to="/shop/sidebar/3cols"
-        class="btn btn-outline-dark-2 btn-round btn-more"
-      >
-        <span>Shop more Outlet deals</span>
-        <i class="icon-long-arrow-right"></i>
+      <router-link to="/shop/sidebar/3cols" class="btn btn-outline-dark-2 btn-round btn-more"
+        style="gap: 5px; font-family: Gilroy-Medium !important; font-weight: 600;">
+        <span>Смотреть все направления</span>
+        <img src="../../../assets/newImg/icons/bl_arrow-right.svg" alt="">
+        <!-- <i class="icon-long-arrow-right"></i> -->
       </router-link>
     </div>
   </div>
