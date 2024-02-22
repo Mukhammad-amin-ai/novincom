23e78ef8e23da5bd159cf5db8dced322839cb52c
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2024-02-22 08:44:07 +0500

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -16,22 +16,20 @@ const router = createRouter({
       component: loginVue,
     },
     {
-
       path: "/product/default/:slug",
       name: "product show",
       component: () => import("@/views/product/centered/_slug.vue"),
     },
-
+    {
       path: "/tickets_search_results",
       name: "tickets search results",
-      component: loginVue
+      component: loginVue,
     },
     {
-      path:"/catalog",
-      name:"catalog",
-      component:loginVue
-    }
-
+      path: "/catalog",
+      name: "catalog",
+      component: loginVue,
+    },
   ],
 });
 
