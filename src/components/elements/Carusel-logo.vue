58c75b708a1547fa5ffc6b94b5e875c13ac637be
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2024-02-24 10:07:52 +0500

    update carousel

--- src/components/elements/Carusel-logo.vue
+++ src/components/elements/Carusel-logo.vue
@@ -1,61 +1,70 @@
 <template>
-    <div>
-        <swiper :slidesPerView="3" :spaceBetween="30" :freeMode="true" :modules="modules"
-            class="mySwiper">
-            <swiper-slide>
-                <router-link to="#" style="cursor: pointer;">
-                    <img src="../../assets/newImg/logotypes/bosch-logo-simple 1.png" alt="">
-                </router-link>
-            </swiper-slide>
-            <swiper-slide>
-                <router-link to="#" style="cursor: pointer;">
-                    <img src="../../assets/newImg/logotypes/ozon.png" alt="">
-                </router-link>
-            </swiper-slide>
-            <swiper-slide>
-                <router-link to="#" style="cursor: pointer;">
-                    <img src="../../assets/newImg/logotypes/xiaomi.png" alt="">
-                </router-link>
-            </swiper-slide>
-            <swiper-slide>
-                <router-link to="#" style="cursor: pointer;">
-                    <img src="../../assets/newImg/logotypes/десткий мир.png" alt="">
-                </router-link>
-            </swiper-slide>
-            <swiper-slide>
-                <router-link to="#" style="cursor: pointer;">
-                    <img src="../../assets/newImg/logotypes/аэрофлот.png" alt="">
-                </router-link>
-            </swiper-slide>
-            <swiper-slide>
-                <router-link to="#" style="cursor: pointer;">
-                    <img src="../../assets/newImg/logotypes/РЖД.png" alt="">
-                </router-link>
-            </swiper-slide>
-        </swiper>
-    </div>
+  <div>
+    <swiper
+      :slidesPerView="4"
+      :spaceBetween="30"
+      :freeMode="true"
+      :modules="modules"
+      :navigation="true"
+      class="mySwiper"
+      :autoplay="true"
+    >
+      <swiper-slide>
+        <router-link to="#" style="cursor: pointer">
+          <img
+            src="../../assets/newImg/logotypes/bosch-logo-simple 1.png"
+            alt=""
+          />
+        </router-link>
+      </swiper-slide>
+      <swiper-slide>
+        <router-link to="#" style="cursor: pointer">
+          <img src="../../assets/newImg/logotypes/ozon.png" alt="" />
+        </router-link>
+      </swiper-slide>
+      <swiper-slide>
+        <router-link to="#" style="cursor: pointer">
+          <img src="../../assets/newImg/logotypes/xiaomi.png" alt="" />
+        </router-link>
+      </swiper-slide>
+      <swiper-slide>
+        <router-link to="#" style="cursor: pointer">
+          <img src="../../assets/newImg/logotypes/десткий мир.png" alt="" />
+        </router-link>
+      </swiper-slide>
+      <swiper-slide>
+        <router-link to="#" style="cursor: pointer">
+          <img src="../../assets/newImg/logotypes/аэрофлот.png" alt="" />
+        </router-link>
+      </swiper-slide>
+      <swiper-slide>
+        <router-link to="#" style="cursor: pointer">
+          <img src="../../assets/newImg/logotypes/РЖД.png" alt="" />
+        </router-link>
+      </swiper-slide>
+    </swiper>
+  </div>
 </template>
 
 <script>
-import { FreeMode, Pagination } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/vue';
+import { FreeMode, Pagination, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/vue";
 
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
 import "swiper/css/navigation";
 export default {
-    components: {
-        Swiper,
-        SwiperSlide,
-    },
-    setup() {
-        return {
-            modules: [FreeMode, Pagination],
-        };
-    }
-}
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [FreeMode, Pagination, Autoplay],
+    };
+  },
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
