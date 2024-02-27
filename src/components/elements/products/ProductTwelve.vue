6dd2365e8101bb1ae41e21baa77f012bb01da51c
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2024-02-27 21:48:56 +0500

    add formatnumber

--- src/components/elements/products/ProductTwelve.vue
+++ src/components/elements/products/ProductTwelve.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="product product-2">
     <figure class="product-media">
-      <span class="product-label label-circle label-new" v-if="product.new">Новинка</span>
-      <span class="product-label label-circle label-sale" v-if="product.featured">Выгодно</span>
-      <span class="product-label label-circle label-top" v-if="product.top">Топ</span>
-      <span class="product-label label-circle label-out" v-if="product.stock === 0">Out Of Stock</span>
+      <span class="product-label label-circle label-new" v-if="product.new"
+        >Новинка</span
+      >
+      <span
+        class="product-label label-circle label-sale"
+        v-if="product.featured"
+        >Выгодно</span
+      >
+      <span class="product-label label-circle label-top" v-if="product.top"
+        >Топ</span
+      >
+      <span
+        class="product-label label-circle label-out"
+        v-if="product.stock === 0"
+        >Out Of Stock</span
+      >
       <router-link :to="'/product/default/' + product.slug">
-        <img :src="`${product.sm_pictures[0].url}`" alt="Product" :width="product.sm_pictures[0].width"
-          :height="product.sm_pictures[0].height" class="product-image" />
-        <img :src="`${product.sm_pictures[1].url}`" alt="Product" :width="product.sm_pictures[1].width"
-          :height="product.sm_pictures[1].height" class="product-image-hover" v-if="product.sm_pictures[1]" />
+        <img
+          :src="`${product.sm_pictures[0].url}`"
+          alt="Product"
+          :width="product.sm_pictures[0].width"
+          :height="product.sm_pictures[0].height"
+          class="product-image"
+        />
+        <img
+          :src="`${product.sm_pictures[1].url}`"
+          alt="Product"
+          :width="product.sm_pictures[1].width"
+          :height="product.sm_pictures[1].height"
+          class="product-image-hover"
+          v-if="product.sm_pictures[1]"
+        />
       </router-link>
       <!-- <div class="product-action-vertical" v-if="product.stock !== 0">
         <router-link to="/shop/wishlist" class="btn-product-icon btn-wishlist added-to-wishlist"
@@ -32,17 +55,21 @@
     <div class="product-body">
       <div class="product-cat">
         <span v-for="(cat, index) of product.category" :key="index">
-          <router-link :to="{ path: '/shop/sidebar/list', query: { category: cat.slug } }">{{ cat.name }}</router-link>
-          {{ index < product.category.length - 1 ? "," : "" }} </span>
+          <router-link
+            :to="{ path: '/shop/sidebar/list', query: { category: cat.slug } }"
+            >{{ cat.name }}</router-link
+          >
+          {{ index < product.category.length - 1 ? "," : "" }}
+        </span>
       </div>
       <h3 class="product-title">
         <router-link :to="'/product/default/' + product.slug">{{
-          product.name 
+          product.name
         }}</router-link>
       </h3>
 
       <div class="product-price" v-if="product.stock == 0" key="outPrice">
-        <span class="out-price">{{ product.price.toFixed(2) }}</span>
+        <span class="out-price">{{ formatNumber(product.price) }}</span>
       </div>
 
       <template v-else>
@@ -54,8 +81,8 @@
             <span class="new-price">{{ minPrice.toFixed(2) }}</span>
             <span class="old-price">{{ maxPrice.toFixed(2) }}</span>
           </div>
-          <div class="product-price" v-else >
-            {{ minPrice.toFixed(2) }}&ndash;{{ maxPrice.toFixed(2) }}
+          <div class="product-price" v-else>
+            {{ formatNumber(minPrice) }}&ndash;{{ formatNumber(maxPrice) }}
           </div>
         </template>
       </template>
@@ -71,7 +98,6 @@
   </div>
 </template>
 <script>
-
 import { mapGetters, mapActions } from "vuex";
 import { baseUrl } from "../../../repositories/repository.js";
 
@@ -111,6 +137,9 @@ export default {
     this.maxPrice = max;
   },
   methods: {
+    formatNumber(number) {
+      return number?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    },
     isInWishlist(product) {
       this.$store.commit("isInWishlist", product);
     },
