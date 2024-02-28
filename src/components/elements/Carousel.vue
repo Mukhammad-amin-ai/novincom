c0870227872ef458194140245e85d65d6c1c1763
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2024-02-28 13:22:50 +0500

    add right swiper item

--- src/components/elements/Carousel.vue
+++ src/components/elements/Carousel.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="carousel">
     <div class="container items-center">
-      <swiper :loop="true" :modules="modules" :navigation="showPagination" :slides-per-view="1" :space-between="150"
-        @slideChange="onSlideChange" @swiper="onSwiper" :autoplay="{
+      <swiper
+        :loop="true"
+        :modules="modules"
+        :navigation="showPagination"
+        :slides-per-view="1"
+        :space-between="150"
+        @slideChange="onSlideChange"
+        @swiper="onSwiper"
+        :autoplay="{
           delay: 2500,
           disableOnInteraction: false,
-        }" :draggable="true">
+        }"
+        :draggable="true"
+      >
         <swiper-slide v-for="slide in data" :key="slide.id">
           <div class="container swiper-item swiper_custom_item items-center">
             <div class="left-swiper-item">
               <div v-if="slide.image" class="right-swiper-item-img">
-                <img format="webp" class="main-img" loading="lazy" :alt="slide.name" :src="slide.image" />
+                <img
+                  format="webp"
+                  class="main-img"
+                  loading="lazy"
+                  :alt="slide.name"
+                  :src="slide.image"
+                />
               </div>
             </div>
-            <div class="right-swiper-item d-flex flex-column justify-content-center">
+            <div
+              class="right-swiper-item d-flex flex-column justify-content-center"
+            >
               <div class="swiper-item-desc">
                 {{ slide.description }}
               </div>
@@ -21,12 +38,24 @@
                 {{ slide.name }}
               </div>
               <div class="w-100 d-flex justify-content-start">
-                <router-link to="/sign_up" class="auth-btn text-white mb-2" style="white-space: nowrap;">Вход /
-                  Регистрация
-                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd"
+                <router-link
+                  to="/sign_up"
+                  class="auth-btn text-white mb-2"
+                  style="white-space: nowrap"
+                  >Вход / Регистрация
+                  <svg
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
                       d="M1 7.99999C1 7.86738 1.05268 7.74021 1.14645 7.64644C1.24021 7.55267 1.36739 7.49999 1.5 7.49999H13.293L10.146 4.35399C10.0521 4.2601 9.99937 4.13277 9.99937 3.99999C9.99937 3.86721 10.0521 3.73988 10.146 3.64599C10.2399 3.5521 10.3672 3.49936 10.5 3.49936C10.6328 3.49936 10.7601 3.5521 10.854 3.64599L14.854 7.64599C14.9006 7.69244 14.9375 7.74761 14.9627 7.80836C14.9879 7.8691 15.0009 7.93422 15.0009 7.99999C15.0009 8.06576 14.9879 8.13088 14.9627 8.19162C14.9375 8.25237 14.9006 8.30754 14.854 8.35399L10.854 12.354C10.7601 12.4479 10.6328 12.5006 10.5 12.5006C10.3672 12.5006 10.2399 12.4479 10.146 12.354C10.0521 12.2601 9.99937 12.1328 9.99937 12C9.99937 11.8672 10.0521 11.7399 10.146 11.646L13.293 8.49999H1.5C1.36739 8.49999 1.24021 8.44731 1.14645 8.35354C1.05268 8.25978 1 8.1326 1 7.99999Z"
-                      fill="white" />
+                      fill="white"
+                    />
                   </svg>
                 </router-link>
               </div>
@@ -79,14 +108,12 @@ export default {
     Swiper,
     SwiperSlide,
   },
-  mounted() {
-
-  },
+  mounted() {},
   setup() {
     const onSwiper = (swiper) => {
       return swiper;
     };
-    const onSlideChange = () => { };
+    const onSlideChange = () => {};
     return {
       onSwiper,
       onSlideChange,
@@ -96,10 +123,6 @@ export default {
 };
 </script>
 <style>
-.right-swiper-item {
-  margin-right: 158px;
-}
-
 .swiper-button-next,
 .swiper-button-prev {
   background: white !important;
